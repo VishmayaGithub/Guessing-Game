0aea75378937054db943ce43eff57e13276e1d52
VishmayaGithub <72074398+VishmayaGithub@users.noreply.github.com>
2021-05-21 10:00:19 +0530

    Create guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -2,20 +2,20 @@ from random import randrange
 random = randrange(15)
 name=input("Enter Your name : ")
 
-print("Hello",name,"welcome to the guessing game. All you need to do is guess a number between 1 and 15")
+print("Hello",name,"welcome to the guessing game.I am thinking of a number between 1 and 15.")
 
 for numberOfGuesses in range(1,6):
     
     guess = int(input("Enter Your guess : "))
     
     if(guess<random):
-        print("Your guess is bigger than",guess)
+        print("Your guess is bigger than ",guess)
     elif(guess>random) :
         print("Your guess is smaller than",guess)   
     else:  break    
 
 if(guess==random):
-    print("Yay you won!!!😁")        
+    print("Yay you won",name," !!!")        
 if(guess!=random):
     print("Sorry you lost",name)            
 
